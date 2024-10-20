7017d6d983bc5a0bbf3df0bf4ec73a95d1e1ad7c
junsu <junsu000306@gmail.com>
2024-10-20 15:53:50 +0900

    프런티 & 백엔드 로그인 cors 추가

--- BE/app.js
+++ BE/app.js
@@ -1,18 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const chatRoutes = require('./routes/chatRoute'); // 챗봇 관련 라우트 가져오기
 const userRoutes = require('./routes/userRoutes'); // 사용자 관련 라우트 가져오기
-//const errorMiddleware = require('./middlewares/errorMiddleware'); // 에러 처리 미들웨어
-
+// const errorMiddleware = require('./middlewares/errorMiddleware'); // 에러 처리 미들웨어
 
 const app = express(); // Express 애플리케이션 생성
+
+// CORS 설정 - 모든 출처 허용
+app.use(cors());  // CORS 설정이 라우트보다 위에 있어야 함
 
 // 미들웨어 설정
 app.use(bodyParser.json()); // JSON 요청 본문 파싱
 app.use(bodyParser.urlencoded({ extended: true })); // URL 인코딩된 데이터 파싱
+app.use(express.json());
 
 // 라우트 설정
-app.use('/chat', chatRoutes); // '/api' 경로에 챗봇 관련 라우트 적용
-app.use('/user', userRoutes); // '/auth' 경로에 사용자 관련 라우트 적용
+app.use('/chat', chatRoutes); // '/chat' 경로에 챗봇 관련 라우트 적용
+app.use('/user', userRoutes); // '/user' 경로에 사용자 관련 라우트 적용
 
 module.exports = app; // Express 앱을 모듈로 내보냄
