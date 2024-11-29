dbad913f1567f7cd3a67afb5393617040053547f
kmspeter <kmspeter@naver.com>
2024-11-29 23:18:45 +0900

    게시판 조회 수정

--- BE/models/dataModel.js
+++ BE/models/dataModel.js
@@ -6,6 +6,7 @@ exports.getSchoolNotices = async () => {
         SELECT idx, title, published_date 
         FROM school_notice 
         ORDER BY published_date DESC
+        LIMIT 50
     `);
     return notices;
 };
@@ -15,6 +16,27 @@ exports.getSchoolNoticeDetail = async (idx) => {
     const [notice] = await db.query(`
         SELECT idx, title, text, published_date 
         FROM school_notice 
+        WHERE idx = ?
+    `, [idx]);
+    return notice[0];
+};
+
+// 외부 공지 목록 조회
+exports.getExternalNotices = async () => {
+    const [notices] = await db.query(`
+        SELECT idx, title, deadline_date 
+        FROM external_notice 
+        ORDER BY deadline_date ASC
+        LIMIT 50
+    `);
+    return notices;
+};
+
+// 외부 공지 상세 조회
+exports.getExternalNoticeDetail = async (idx) => {
+    const [notice] = await db.query(`
+        SELECT idx, title, text, deadline_date 
+        FROM external_notice 
         WHERE idx = ?
     `, [idx]);
     return notice[0];
